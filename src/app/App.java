0427d5076783dc17b54f04d2800f9ec313d63d45
desferreira <diego.ferreira@justa.com.vc>
2020-02-26 12:33:13 -0300

    :tada: Dados de acesso JDBC
    Signed-off-by: desferreira <diego.ferreira@justa.com.vc>

---
+++ src/app/App.java
@@ -0,0 +1,18 @@
+package app;
+
+import db.DB;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class App {
+    public static void main(String[] args){
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+        PreparedStatement ps = null;
+
+    }
+}
